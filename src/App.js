1ba15e484b2939b1e624e357b89a9d4130e35765
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-06-25 14:57:29 +0300

    imported switch and route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./components/HomePage";
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Cars from './components/Cars';
+
 
 function App() {
   return (    
-      <Homepage/>
+        <>
+          <Navbar/>
+          <div>
+            <Switch>
+              <Route exact path="/">
+                  <Homepage/>
+              </Route>
+              <Route exact path="/about">
+                  <About/>
+              </Route>
+              <Route exact path="/cars">
+                  <Cars/>
+              </Route>
+            </Switch>
+          </div>      
+      </>      
+      
   )
 }
 
