087f13903b55042fcc3ae8b21311eb3eb6b36848
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-06-28 12:20:10 +0300

    changed id to grandmaster

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,9 @@
 import React from "react";
+import pic1 from "/home/binamin/Desktop/Car-treck/car-treck/src/statics/1968 Dodge Charger.jpeg"
 
 function Homepage() {
   return (
-    <div id="grandmaster">
+    <div >
       
         <div id="master">
             <h1>
@@ -10,6 +11,9 @@ function Homepage() {
             </h1>
             <div>
                 <img src="src/statics/1968 Dodge Charger.jpeg" alt="cartreck1"/>
+                <img src="car-treck/src/statics/Car Porn™ on Twitter.jpeg" alt="cartreck2"/>
+                <img src="car-treck/src/statics/Premium Vector _ Modern and sport car illustration.jpeg" alt="cartreck3"/>
+
             </div>
         </div>
     </div>
