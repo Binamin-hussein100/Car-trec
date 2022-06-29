046fd0239e9af393afd2611b74429c6385d54007
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-06-29 08:43:46 +0300

    change page format

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from 'react-router-dom'
 import pic1 from "../Premium_Vector___Modern_and_sport_car_illustration-removebg-preview.png"
 
 function Homepage() {
@@ -11,10 +12,18 @@ function Homepage() {
                 <div>
                   <img className="ginda" src={pic1} alt="cartreck1"/>            
                 </div>
-                <div>
-                    <h1 id="ninja">
-                        Car-Treck
+                <div id="ninja">
+                    <h1 >
+                        Car-<span id="trec">Treck</span>
                     </h1>
+                    <p>
+                      THE BEST CAR DEALS AND ONLINE SHOWROOM
+                    </p>
+                    <NavLink to="/cars" ><button id="shop">
+                    <span className="actual-text">&nbsp;SHOP&nbsp;</span>
+    <span className="hover-text" aria-hidden="true">&nbsp;SHOP&nbsp;</span>
+                    </button></NavLink>
+                    
                 </div>
             </div>
         </div>
