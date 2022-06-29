4e517bcdc3eeeaf10795740d9a56555392994fdf
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-06-29 08:42:59 +0300

    passed in a prop

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,7 +1,8 @@
 import React,{useState,useEffect} from "react";
+import { Button } from 'react-bootstrap';
 
-function Cars(){
-    const [cars, setCars] = useState([])
+
+function Cars({cars,setCars}){
 
     console.log("=======",cars)
 
@@ -12,11 +13,34 @@ function Cars(){
             console.log(data)
             setCars(data)})
     },[])
+
     return (
-        <div>
-            <h1>Master</h1>
+        <div id="display2">
             <ul>
-                { cars?.map(car => <li key={car.id}> {car.model }</li>
+                { cars?.map(car => (
+                <li key={car.id}> 
+                    <div id="display">
+                        <div className="danger">
+                            <img src={car.pic} alt="car"/>
+                            
+                        </div>
+                        <div className="danger">
+                            <h3>CAR MODEL : {car.model }</h3>
+                            <h4>CAR NAME :  {car.cname}</h4>
+                            <h4>YEAR :  {car.modelYear}</h4>
+                            <h4>PRICE :  {car.price}</h4>
+
+                            <a class="fancy" href="#">
+                                <span class="top-key"></span>
+                                <span class="text">Buy the Car</span>
+                                <span class="bottom-key-1"></span>
+                                <span class="bottom-key-2"></span>
+                            </a>
+                        </div>
+
+                    </div>
+                </li>
+                )
                 )}
             </ul>
         </div>
